4c297dd1e6bacd7403c63ab5736161eaa722020d
Bantamlak <neoluka315@gmail.com>
2023-03-01 12:36:04 +0000

    the new node has returned

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,7 +4,7 @@
  * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: is a pointer to the node to insert the left-child in
  * @value: is the value to store in the new node
- * 
+ *
  * Return: a pointer to the created node, or NULL on failure
  *			or if parent is NULL
  */
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-	
+
 	new_node = binary_tree_node(parent, value);
 	if (parent->left != NULL)
 	{
@@ -26,5 +26,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	else
 		parent->left = new_node;
 
-	return (parent->left);
+	return (new_node);
 }
